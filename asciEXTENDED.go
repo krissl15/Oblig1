577d63bc2001e21b432c2d16bcb31c24a8797290
krissl15 <35600843+krissl15@users.noreply.github.com>
2018-02-05 15:49:23 -0800

    Update asciEXTENDED.go

--- asciEXTENDED.go
+++ asciEXTENDED.go
@@ -6,6 +6,20 @@ import (
 	"log"
 )
 
+//////////////////LØSNING/////////////
+func main() {
+	src := []byte("20e282ac20c3b720c2be20646f6c6c617220")
+
+	dst := make([]byte, hex.DecodedLen(len(src)))
+	n, err := hex.Decode(dst, src)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", dst[:n])
+
+}
+/////////////////////////LØSNING SLUTT////////////////////
 func main() {
 	const s = "e282ac"//<-- skriv in bytes i hex her
 	decoded, err := hex.DecodeString(s)
