8043be0e306fd87ee495662e6eb2af90b99ea538
krissl15 <35600843+krissl15@users.noreply.github.com>
2018-02-07 05:26:54 -0800

    Update sort.go

--- sort.go
+++ sort.go
@@ -1,32 +1,65 @@
-package main
+package algorithms
 
-import "fmt"
+// Les https://en.wikipedia.org/wiki/Bubble_sort
 
-func swap(list []int, a, b int){
-	tem := list[b]
-	list[b]=list[a]
-	list[a]=tem
+////////////////////opg2a///////////////
+func swap(list []int, i, j int) {
+	temp := list[j]
+	list[j] = list[i]
+	list[i] = temp
 }
 
-
-func sort(list []int){
-
+func Bubble_sort_modified(list []int) {
 	sorted := true
 	for sorted {
-		sorted=false
-		for i := 0; i < len(list) - 1; i++{
-			if list[i+1] < list[i]{
+		sorted = false
+		for i := 0; i < len(list)-1; i++ {
+			if list[i+1] < list[i] {
 				swap(list, i, i+1)
-				sorted=true
+				sorted = true
+			}
+		}
+	}
+}
+/////////////////opg2a slutt//////////////
+
+// Implementering av Bubble_sort algoritmen
+func BubbleSort(list []int) {
+	// find the length of list n
+	n := len(list)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-1; j++ {
+			if list[j] > list[j+1] {
+				temp := list[j+1]
+				list[j+1] = list[j]
+				list[j] = temp
 			}
 		}
 	}
 }
 
-func main(){
+// Implementering av Quicksort algoritmen
+func QuickSort(values []int) {
+	qsort(values, 0, len(values)-1)
+}
 
-		liste := []int{5, 2, 7, 3, 87, 2, 3, 4, 0, 1}
-	fmt.Println("usortert:", liste)
-	sort(liste)
-	fmt.Println("vingardium leviosa!", liste)
+func qsort(values []int, l int, r int) {
+	if l >= r {
+		return
+	}
+
+	pivot := values[l]
+	i := l + 1
+
+	for j := l; j <= r; j++ {
+		if pivot > values[j] {
+			values[i], values[j] = values[j], values[i]
+			i++
+		}
+	}
+
+	values[l], values[i-1] = values[i-1], pivot
+
+	qsort(values, l, i-2)
+	qsort(values, i, r)
 }
