1bbd44b39d7f18a57532a98906dfc2c00f2f17cd
krissl15 <35600843+krissl15@users.noreply.github.com>
2018-02-09 13:52:26 +0100

    Update asciiEKS3.go

--- Oblig1/asciiEKS3.go
+++ Oblig1/asciiEKS3.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	t1 := []byte{34, 190, 32, 247, 32, 100, 111, 108, 108, 97, 114, 34}
+	t1 := []byte{34, 190, 32, 247, 32, 100, 111, 108, 108, 97, 114, 34} // codepoints til "¾ ÷ dollar"
 	for i := 0; i < len(t1); i++ {
-		fmt.Printf("%c", t1[i])
+		fmt.Printf("%c", t1[i])  // &c viser symbolet til codepointet t[1].
 	}
 
 }
