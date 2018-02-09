800ccdc76c937779336eb6973cbec86df22c5cef
krissl15 <35600843+krissl15@users.noreply.github.com>
2018-02-09 11:52:12 +0100

    Update iso_test.go

--- Oblig1/iso_test.go
+++ Oblig1/iso_test.go
@@ -1,24 +1,36 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func asciiTest(j string) bool {
 	for _, i := range j {
-		if i < 127 || i> 255 {
+		if i < 127 || i > 255  {
 			return true
 		}
 	}
 	return false
 }
 
-func testExtendedASCII(t * testing.B){
-	g := ExtendedASCIIText()
-	if asciiTest(g) == false {
-		t.Errorf("fail")
+func TestExtendedASCII(t *testing.T) {
+
+	asciiR := []rune{34, 8364, 32, 190, 32, 247, 32, 100, 111, 108, 108, 97, 114, 34}
+	asciiS := string(asciiR)
+	asciiByteSlice := []byte(asciiS)
+
+	g := ExtendedASCIIText(asciiByteSlice)
+
+
+	for _, d := range g { //<-- Skriv in strings som skal testes her.
+		h := string(d)
+		if asciiTest(h) == true {
+
+			t.Errorf("%s %v", h, "This string is  NOT extended ASCII")
+		} else {
+			t.Errorf("%s %v", h, "This string IS from the extended ASCII")
+
+		}
 	}
+
 }
-
-
-
-
-
