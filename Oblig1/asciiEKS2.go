acbe88971d46190412ff253627843d24dd7867dd
krissl15 <35600843+krissl15@users.noreply.github.com>
2018-02-14 01:41:01 -0800

    Update asciiEKS2.go

--- Oblig1/asciiEKS2.go
+++ Oblig1/asciiEKS2.go
@@ -25,7 +25,7 @@ func byteToHex() {
 * Å bruke disse to funksjonene om hverandre gir en grei løsning på oppgaven, men krever ingen forståelse av unicode. 
 */
 func main() {
-	src := []byte("20e282ac20c3b720c2be20646f6c6c617220") // Bruker en annen f
+	src := []byte("20e282ac20c3b720c2be20646f6c6c617220") // Bruker input fra funksjonen byteToHex
 
 	dst := make([]byte, hex.DecodedLen(len(src)))
 	n, err := hex.Decode(dst, src)
